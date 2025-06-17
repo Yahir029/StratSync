c1722778204b1618685a912c5aaeca51c6be1d61
Setsa2112 <setsahid@gmail.com>
2025-06-16 21:49:02 -0600

    PRUEBA1

--- StratSync/server/config/config.js
+++ StratSync/server/config/config.js
@@ -14,16 +14,41 @@ module.exports = {
   }
 };
 */
-const config = {
-  development: {
-   username: 'stratsync_user',
-    password: 'stratDBconect01$',
-    database: 'stratsync',
-    host: 'localhost',
-    dialect: 'postgres',
+//const config = {
+  //development: {
+   //username: 'stratsync_user',
+    //password: 'stratDBconect01$',
+    //database: 'stratsync',
+    //host: 'localhost',
+    //dialect: 'postgres',
     // otros campos opcionales como logging, port, etc.
-  },
+  //},
   // otros entornos si los necesitas
+//};
+
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'stratsync_user',
+    password: process.env.DB_PASSWORD || 'stratDBconect01$',
+    database: process.env.DB_NAME || 'stratsync',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
+
 
 export default config;
